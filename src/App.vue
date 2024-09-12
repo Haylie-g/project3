1844f21231ec8fa5e9918b6de9e313f75e9a6eb8
Haylie Guzman <hguzma15@stu.southtexascollege.edu>
2024-09-12 12:23:51 -0500

    set up Recipie Maker vue with form structure

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,37 @@
 </script>
 
 <template>
- 
+ <div>
+    <header>
+      <h1>Recipe Maker &#127856;</h1>
+    </header>
+
+    <main>
+      <section id="recipe-input">
+        <h2>Enter Your Recipe</h2>
+        <form @submit.prevent="generateRecipe">
+          <fieldset>
+            <legend>Recipe Name</legend>
+            <input v-model="recipeName" type="text" placeholder="Enter recipe name..." required>
+          </fieldset>
+
+          <fieldset>
+            <legend>Ingredients</legend>
+            <textarea v-model="ingredients" rows="5" cols="50" placeholder="Enter ingredients here..." required></textarea>
+          </fieldset>
+
+          <fieldset>
+            <legend>Directions</legend>
+            <textarea v-model="directions" rows="5" cols="50" placeholder="Enter directions here..." required></textarea>
+          </fieldset>
+
+          <button type="submit">Generate Recipe</button>
+        </form>
+
+      </section>
+    </main>
+ </div>
+ </template>
 
 
 
